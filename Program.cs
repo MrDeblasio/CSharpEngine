01d3658a42a6bf6a104815bc657c361577ef1aad
Deblasio <43344427+MrDeblasio@users.noreply.github.com>
2021-11-18 13:03:16 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpEngineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
